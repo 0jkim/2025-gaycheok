d38d3313a5c9994be911169bba9899c6f1756146
0jkim <kimyeongjin9060@gmail.com>
2025-11-20 13:52:01 +0000

    init nr, opengym

---
+++ scratch/scenario.cc
@@ -0,0 +1,5 @@
+/**
+ * This scenario file show trade-off releationship of harq feedback and energy efficiency.
+ * Specifically, scenario is based on 3GPP TR 38.913 deployment sceanrios(urban macro)
+ * 
+ */
